d9c3cec0c86e163a6f347df53512dd620dd22b77
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2024-10-22 10:20:59 +0100

    added preliminary NN files

--- include/TrackOne.cpp
+++ include/TrackOne.cpp
@@ -5,4 +5,15 @@
 #include "../lib/TrackOne.h"
 
 namespace Arena {
+
+    TrackOne::TrackOne() {
+        params.fill(0ull);
+    }
+
+    // creates neural network from parameters
+    // and returns output
+    float TrackOne::runNeuralNetwork(uint8_t input) {
+
+    }
+
 } // Arena
--- lib/TrackOne.h
+++ lib/TrackOne.h
@@ -1,6 +1,9 @@
 //
 // Created by ron0 on 20/10/24.
 //
+#include <array> // std::array
+#include <stdint.h> // uint64_t
+
 
 #ifndef TECHARENA2024_TRACKONE_H
 #define TECHARENA2024_TRACKONE_H
@@ -8,6 +11,15 @@
 namespace Arena {
 
     class TrackOne {
+    public:
+        TrackOne();
+        ~TrackOne();
+
+        // creates micro feedforward neural network
+        void createNeuralNetwork();
+
+    private:
+        std::array<uint64_t, 4> params;
 
     };
 
